bc8b31604028c31d93dbb425d49599e51a9a3184
mbien <mbien@5e721eb9-562f-0410-8d64-e26d913fd139>
2007-06-28 16:17:36 +0000

    initial commit of JOGL utilities (seperated from glsl-compiler-editor-support module)
    git-svn-id: https://netbeans-opengl-pack.dev.java.net/svn/netbeans-opengl-pack/trunk@68 5e721eb9-562f-0410-8d64-e26d913fd139

---
+++ jogl-utils/src/com/mbien/engine/util/GLRunnable.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 29. March 2007, 00:05
+*/
+package com.mbien.engine.util;
+
+import javax.media.opengl.GLContext;
+
+/**
+ * @author Michael Bien
+ */
+public interface GLRunnable {
+    
+    public void run(GLContext context);
+
+}
