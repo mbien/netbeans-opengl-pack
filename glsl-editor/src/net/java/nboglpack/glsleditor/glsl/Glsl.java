753adadaff12d18935e4789934f198a44e0eea74
mbien <mbien@5e721eb9-562f-0410-8d64-e26d913fd139>
2007-10-30 09:40:29 +0000

    bugfix for nb beta2 -context.getTokenSequence(); has been removed from GLF
    git-svn-id: https://netbeans-opengl-pack.dev.java.net/svn/netbeans-opengl-pack/trunk@299 5e721eb9-562f-0410-8d64-e26d913fd139

--- glsl-editor/src/net/java/nboglpack/glsleditor/glsl/Glsl.java
+++ glsl-editor/src/net/java/nboglpack/glsleditor/glsl/Glsl.java
@@ -8,13 +8,14 @@
 package net.java.nboglpack.glsleditor.glsl;
 
 import java.util.List;
+import javax.swing.text.AbstractDocument;
 import net.java.nboglpack.glsleditor.lexer.GlslTokenId;
 import org.netbeans.api.languages.ASTItem;
 import org.netbeans.api.languages.ASTNode;
 import org.netbeans.api.languages.ASTToken;
-import org.netbeans.api.languages.Context;
 import org.netbeans.api.languages.SyntaxContext;
 import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.api.lexer.TokenUtilities;
 
@@ -34,73 +35,83 @@ public final class Glsl {
      */
     public static final String createFunctionDeclarationString(SyntaxContext context) {
         
-        TokenSequence sequence = context.getTokenSequence();        
+        AbstractDocument document = (AbstractDocument)context.getDocument();
+        document.readLock();
         
         StringBuilder sb = new StringBuilder();
-        sb.append("<html>");
         
-        int moved = 0;
-        while(sequence.movePrevious() && isIgnoredToken(sequence.token()))
-            moved++;
-        
-        String type = sequence.token().toString();
-        while(moved-- >= 0)
-            sequence.moveNext();
-        
-        // append function name
-        sb.append(sequence.token().text());
-        
-        while(!TokenUtilities.equals(sequence.token().text(), "("))
-            sequence.moveNext();
-
-        sb.append("(");
-        
-        Token token;
-        boolean first = true;
-        while(sequence.moveNext() && !TokenUtilities.equals(sequence.token().text(), ")")) {
+        try {
             
-            token = sequence.token();
-            
-            if(!isIgnoredToken(token)) {
-                
-                if(first) {
-                    sb.append(KEYWORD_FONT_COLOR);
-                }else if(token.id() != GlslTokenId.COMMA && token.id() != GlslTokenId.BRACKET && token.id() != GlslTokenId.INTEGER_LITERAL) {
-                    sb.append(" ");
-                }
-                
-                if(!TokenUtilities.equals(token.text(), "void")) {
+            TokenSequence sequence = TokenHierarchy.get(context.getDocument()).tokenSequence();
+           
+            sb.append("<html>");
 
-                    moved = 0;
-                    while(sequence.moveNext() && isIgnoredToken(sequence.token())) 
-                        moved++;
-                    
-                    if(sequence.token().id() == GlslTokenId.COMMA || TokenUtilities.equals(sequence.token().text(), ")"))
-                        sb.append("</font>");
-                    
-                    while(moved-- >= 0)
-                        sequence.movePrevious();
-                    
-                    sb.append(token.text());
-                    
-                    if(token.id() == GlslTokenId.COMMA) 
+            int moved = 0;
+            while(sequence.movePrevious() && isIgnoredToken(sequence.token()))
+                moved++;
+
+            String type = sequence.token().toString();
+            while(moved-- >= 0)
+                sequence.moveNext();
+
+            // append function name
+            sb.append(sequence.token().text());
+
+            while(!TokenUtilities.equals(sequence.token().text(), "("))
+                sequence.moveNext();
+
+            sb.append("(");
+
+            Token token;
+            boolean first = true;
+            while(sequence.moveNext() && !TokenUtilities.equals(sequence.token().text(), ")")) {
+
+                token = sequence.token();
+
+                if(!isIgnoredToken(token)) {
+
+                    if(first) {
                         sb.append(KEYWORD_FONT_COLOR);
+                    }else if(token.id() != GlslTokenId.COMMA && token.id() != GlslTokenId.BRACKET && token.id() != GlslTokenId.INTEGER_LITERAL) {
+                        sb.append(" ");
+                    }
+
+                    if(!TokenUtilities.equals(token.text(), "void")) {
+
+                        moved = 0;
+                        while(sequence.moveNext() && isIgnoredToken(sequence.token())) 
+                            moved++;
+
+                        if(sequence.token().id() == GlslTokenId.COMMA || TokenUtilities.equals(sequence.token().text(), ")"))
+                            sb.append("</font>");
+
+                        while(moved-- >= 0)
+                            sequence.movePrevious();
+
+                        sb.append(token.text());
+
+                        if(token.id() == GlslTokenId.COMMA) 
+                            sb.append(KEYWORD_FONT_COLOR);
+                    }
+
+                    first = false;
                 }
-                
-                first = false;
+
             }
+
+            sb.append("</font>)");
+
+            if(!"void".equals(type)) {
+                sb.append(" : ");
+                sb.append(KEYWORD_FONT_COLOR);
+                sb.append(type);
+                sb.append("</font>");
+            }
+            sb.append("</html>");
             
-        }
-        
-        sb.append("</font>)");
-        
-        if(!"void".equals(type)) {
-            sb.append(" : ");
-            sb.append(KEYWORD_FONT_COLOR);
-            sb.append(type);
-            sb.append("</font>");
-        }
-        sb.append("</html>");
+        } finally {
+          document.readUnlock();
+        }         
         
         return sb.toString();
     }
@@ -111,75 +122,83 @@ public final class Glsl {
      */
     public static final String createFieldDeclarationString(SyntaxContext context) {
         
-        TokenSequence sequence = context.getTokenSequence();
+        AbstractDocument document = (AbstractDocument)context.getDocument();
+        document.readLock();
         
         StringBuilder sb = new StringBuilder();
         
-        sb.append("<html>");
-        sb.append(sequence.token().text());
-        sb.append(KEYWORD_FONT_COLOR);
-        sb.append(" :");
-        
-        int insertIndex = sb.length();
-        
-        // read forward
-        int moved = 0;
-        Token token;
-        while(     sequence.moveNext()
-                && sequence.token().id() != GlslTokenId.SEMICOLON
-                && sequence.token().id() != GlslTokenId.COMMA
-                && sequence.token().id() != GlslTokenId.EQ  ) {
-            token = sequence.token();
-            if(!isIgnoredToken(token)) 
-                sb.append(token);
-            moved++;
-        }
-        while(moved-- >= 0)
-            sequence.movePrevious();
-        
-        // read backwards throw the declaration
-        boolean skipToken = false;
-        
-        while(     sequence.movePrevious() 
-                && sequence.token().id() != GlslTokenId.SEMICOLON 
-                && sequence.token().id() != GlslTokenId.END_OF_LINE ) {
-            
-            token = sequence.token();
-            
-            if(!isIgnoredToken(token)) {
-                
-                // we have a struct declaration; skip everything between { }
-                if(token.id() == GlslTokenId.BRACE && TokenUtilities.equals(token.text(), "}")) {
-                    movePreviousUntil(sequence, GlslTokenId.BRACE, "}", "{");
-                    continue;
-                }                
-                
-                // skip token in case of an comma seperated identifier list
-                if(skipToken) {
-                    if(     token.id() == GlslTokenId.BRACKET
-                         && TokenUtilities.equals(token.text(), "]")    ) {
-                        movePreviousUntil(sequence, GlslTokenId.BRACKET, "]", "[");
-                        skipToken = false;
-                    }else {
-                        skipToken = false;
-                    }
-                    continue;
-                }
-                
-                if(token.id() == GlslTokenId.COMMA) {
-                    skipToken = true;
-                    continue;
-                }
-                
-                if(!TokenUtilities.equals(token.text(), "struct")) {
-                    sb.insert(insertIndex, token.text());
-                    sb.insert(insertIndex, " ");
-                }
+        try {
+            TokenSequence sequence = TokenHierarchy.get(context.getDocument()).tokenSequence();
+
+            sb.append("<html>");
+            sb.append(sequence.token().text());
+            sb.append(KEYWORD_FONT_COLOR);
+            sb.append(" :");
+
+            int insertIndex = sb.length();
+
+            // read forward
+            int moved = 0;
+            Token token;
+            while(     sequence.moveNext()
+                    && sequence.token().id() != GlslTokenId.SEMICOLON
+                    && sequence.token().id() != GlslTokenId.COMMA
+                    && sequence.token().id() != GlslTokenId.EQ  ) {
+                token = sequence.token();
+                if(!isIgnoredToken(token)) 
+                    sb.append(token);
+                moved++;
             }
-            
-        }
+            while(moved-- >= 0)
+                sequence.movePrevious();
+
+            // read backwards throw the declaration
+            boolean skipToken = false;
+
+            while(     sequence.movePrevious() 
+                    && sequence.token().id() != GlslTokenId.SEMICOLON 
+                    && sequence.token().id() != GlslTokenId.END_OF_LINE ) {
+
+                token = sequence.token();
+
+                if(!isIgnoredToken(token)) {
+
+                    // we have a struct declaration; skip everything between { }
+                    if(token.id() == GlslTokenId.BRACE && TokenUtilities.equals(token.text(), "}")) {
+                        movePreviousUntil(sequence, GlslTokenId.BRACE, "}", "{");
+                        continue;
+                    }                
+
+                    // skip token in case of an comma seperated identifier list
+                    if(skipToken) {
+                        if(     token.id() == GlslTokenId.BRACKET
+                             && TokenUtilities.equals(token.text(), "]")    ) {
+                            movePreviousUntil(sequence, GlslTokenId.BRACKET, "]", "[");
+                            skipToken = false;
+                        }else {
+                            skipToken = false;
+                        }
+                        continue;
+                    }
+
+                    if(token.id() == GlslTokenId.COMMA) {
+                        skipToken = true;
+                        continue;
+                    }
+
+                    if(!TokenUtilities.equals(token.text(), "struct")) {
+                        sb.insert(insertIndex, token.text());
+                        sb.insert(insertIndex, " ");
+                    }
+                }
+
+            }
+
+            sb.append("</font></html>");
+        } finally {
+            document.readUnlock();
+        } 
         
-        sb.append("</font></html>");
         
         return sb.toString();
     }
