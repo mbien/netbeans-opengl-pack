fece70c2d5676b67dbcb474901081a25413a6313
mbien <mbien@5e721eb9-562f-0410-8d64-e26d913fd139>
2008-04-09 19:02:39 +0000

    fixed some compiletime warnings
    git-svn-id: https://netbeans-opengl-pack.dev.java.net/svn/netbeans-opengl-pack/trunk@382 5e721eb9-562f-0410-8d64-e26d913fd139

--- jogl-project/src/net/java/nboglpack/joglproject/ProjectWizardPanel.java
+++ jogl-project/src/net/java/nboglpack/joglproject/ProjectWizardPanel.java
@@ -18,85 +18,85 @@ public class ProjectWizardPanel implements WizardDescriptor.Panel,
 	WizardDescriptor.ValidatingPanel, WizardDescriptor.FinishablePanel
 {
 	
-	private WizardDescriptor wizardDescriptor;
-	private ProjectPanelVisual component;
-	
-	/** Creates a new instance of templateWizardPanel */
-	public ProjectWizardPanel()
-	{
-	}
-	
-	public Component getComponent()
-	{
-		if (component == null)
-		{
-			component = new ProjectPanelVisual(this);
-			component.setName(NbBundle.getMessage(ProjectWizardPanel.class, "LBL_CreateProjectStep"));
-		}
-		return component;
-	}
-	
-	public HelpCtx getHelp()
-	{
-		return new HelpCtx(ProjectWizardPanel.class);
-	}
-	
-	public boolean isValid()
-	{
-		getComponent();
-		return component.valid(wizardDescriptor);
-	}
-	
-	private final Set/*<ChangeListener>*/ listeners = new HashSet(1);
-	public final void addChangeListener(ChangeListener l)
-	{
-		synchronized (listeners)
-		{
-			listeners.add(l);
-		}
-	}
-	public final void removeChangeListener(ChangeListener l)
-	{
-		synchronized (listeners)
-		{
-			listeners.remove(l);
-		}
-	}
-	protected final void fireChangeEvent()
-	{
-		Iterator it;
-		synchronized (listeners)
-		{
-			it = new HashSet(listeners).iterator();
-		}
-		ChangeEvent ev = new ChangeEvent(this);
-		while (it.hasNext())
-		{
-			((ChangeListener) it.next()).stateChanged(ev);
-		}
-	}
-	
-	public void readSettings(Object settings)
-	{
-		wizardDescriptor = (WizardDescriptor) settings;
-		component.read(wizardDescriptor);
-	}
-	
-	public void storeSettings(Object settings)
-	{
-		WizardDescriptor d = (WizardDescriptor) settings;
-		component.store(d);
-	}
-	
-	public boolean isFinishPanel()
-	{
-		return true;
-	}
-	
-	public void validate() throws WizardValidationException
-	{
-		getComponent();
-		component.validate(wizardDescriptor);
-	}
+    private WizardDescriptor wizardDescriptor;
+    private ProjectPanelVisual component;
+    private final Set<ChangeListener> listeners = new HashSet<ChangeListener>(1);
+
+    /** Creates a new instance of templateWizardPanel */
+    public ProjectWizardPanel()
+    {
+    }
+
+    public Component getComponent()
+    {
+        if (component == null)
+        {
+            component = new ProjectPanelVisual(this);
+            component.setName(NbBundle.getMessage(ProjectWizardPanel.class, "LBL_CreateProjectStep"));
+        }
+        return component;
+    }
+
+    public HelpCtx getHelp()
+    {
+        return new HelpCtx(ProjectWizardPanel.class);
+    }
+
+    public boolean isValid()
+    {
+        getComponent();
+        return component.valid(wizardDescriptor);
+    }
+
+    public final void addChangeListener(ChangeListener l)
+    {
+        synchronized (listeners)
+        {
+            listeners.add(l);
+        }
+    }
+    public final void removeChangeListener(ChangeListener l)
+    {
+        synchronized (listeners)
+        {
+            listeners.remove(l);
+        }
+    }
+    protected final void fireChangeEvent()
+    {
+        Iterator<ChangeListener> it;
+        synchronized (listeners)
+        {
+            it = new HashSet<ChangeListener>(listeners).iterator();
+        }
+        ChangeEvent ev = new ChangeEvent(this);
+        while (it.hasNext())
+        {
+            it.next().stateChanged(ev);
+        }
+    }
+
+    public void readSettings(Object settings)
+    {
+        wizardDescriptor = (WizardDescriptor) settings;
+        component.read(wizardDescriptor);
+    }
+
+    public void storeSettings(Object settings)
+    {
+        WizardDescriptor d = (WizardDescriptor) settings;
+        component.store(d);
+    }
+
+    public boolean isFinishPanel()
+    {
+        return true;
+    }
+
+    public void validate() throws WizardValidationException
+    {
+        getComponent();
+        component.validate(wizardDescriptor);
+    }
 	
 }
