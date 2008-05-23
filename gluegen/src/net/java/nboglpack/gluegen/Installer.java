001dcfb7aa140d8f029835afa010f64e689d58ba
mbien <mbien@5e721eb9-562f-0410-8d64-e26d913fd139>
2008-05-23 21:17:11 +0000

    initial commit of gluegen library wrapper module now seperated from jogl
    git-svn-id: https://netbeans-opengl-pack.dev.java.net/svn/netbeans-opengl-pack/trunk@410 5e721eb9-562f-0410-8d64-e26d913fd139

---
+++ gluegen/src/net/java/nboglpack/gluegen/Installer.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 23. May 2008
+ */
+package net.java.nboglpack.gluegen;
+
+import java.io.File;
+import java.io.InputStream;
+import net.java.nativelibsupport.LibDeploymentException;
+import net.java.nativelibsupport.NativeLibSupport;
+import org.openide.modules.InstalledFileLocator;
+import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
+
+/**
+ * Installes GlueGens platform dependent libraries.
+ * @author Michael Bien
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        File gluegenDistFolder = InstalledFileLocator.getDefault().locate("gluegen-runtime", "com.sun.gluegen", false);
+        try {
+            InputStream stream = this.getClass().getResourceAsStream("gluegen-natives-config.xml");
+            NativeLibSupport.deploy("gluegen-rt", stream, gluegenDistFolder);
+        } catch (LibDeploymentException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+}
