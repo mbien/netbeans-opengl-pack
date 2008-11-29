31ea59e7a82f43f31f4ea50ed4b9b3bec9741684
mbien <mbien@5e721eb9-562f-0410-8d64-e26d913fd139>
2008-11-29 13:23:12 +0000

    cleanup of layer.xml, moved sample projects to better location in new project wizzard.
    git-svn-id: https://netbeans-opengl-pack.dev.java.net/svn/netbeans-opengl-pack/trunk@495 5e721eb9-562f-0410-8d64-e26d913fd139

--- native-lib-support/src/net/java/nativelibsupport/NativeLibSupport.java
+++ native-lib-support/src/net/java/nativelibsupport/NativeLibSupport.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.java.nativelibsupport;
 
 import java.io.File;
@@ -27,7 +22,7 @@ import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.JarFileSystem;
 
 /**
- * Utility class for native librarie deployment.
+ * Utility class for native library deployment.
  * @author Michael Bien
  */
 public class NativeLibSupport {
--- native-lib-support/test/unit/src/net/java/nativelibsupport/NativeLibSupportTest.java
+++ native-lib-support/test/unit/src/net/java/nativelibsupport/NativeLibSupportTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.java.nativelibsupport;
 
 import java.lang.reflect.InvocationTargetException;
