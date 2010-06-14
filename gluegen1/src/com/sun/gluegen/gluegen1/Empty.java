8c6ab47e6ad061e67feb7f874cd883c4376d07df
Michael Bien <mbien@fh-landshut.de>
2010-06-14 14:32:21 +0200

    removed all jogl1 projects.

--- gluegen1/src/com/sun/gluegen/gluegen1/Empty.java
+++
@@ -1,7 +0,0 @@
-package com.sun.gluegen.gluegen1;
-
-/**
- * Empty class as placeholder. It is apparently not possible to declare a dependency to a module without a class.
- * @author Michael Bien
- */
-public class Empty {}
--- jogl1/src/javax/media/opengl/jogl1/Empty.java
+++
@@ -1,7 +0,0 @@
-package javax.media.opengl.jogl1;
-
-/**
- * Empty class as placeholder. It is apparently not possible to declare a dependency to a module without a class.
- * @author Michael Bien
- */
-public class Empty {}
